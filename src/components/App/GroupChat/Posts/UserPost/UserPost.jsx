b7cba223aa07d568efd47f92f73ef446af9a75e2
Aaron Conklin <conklin.aaron@gmail.com>
2016-12-14 15:06:08 -0500

    More css

--- src/components/App/GroupChat/Posts/UserPost/UserPost.jsx
+++ src/components/App/GroupChat/Posts/UserPost/UserPost.jsx
@@ -5,10 +5,12 @@ class UserPost extends Component {
   render() {
     return (
       <div className={styles["user-post"]}>
-        <img id={styles["prof-pic"]} src={this.props.prof_pic} alt="" />
+        <img id={styles["prof-pic"]} src={this.props.prof_pic} />
         <div className={styles["post-content"]}>
-          <p>{this.props.text}</p>
-          <img src={this.props.image} alt="post-image" />
+          <div className={styles["text"]}>
+            <p>{this.props.text}</p>
+          </div>
+          <img src={this.props.image} />
         </div>
         <button onClick={() => this.props.handleDeletePost(this.props.post_id)}>X</button>
       </div>
